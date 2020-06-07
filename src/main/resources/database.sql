06d34ea5e414cda53aa2bdb97fefd9a77f375e75
Prajakta Rodrigues <prajaktarodrigues@gmail.com>
2020-06-06 23:03:19 -0400

    database script updated

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -4,10 +4,8 @@ CREATE TABLE quiz.User (
     id INT PRIMARY KEY,
     email VARCHAR(30) unique,
     name VARCHAR(50) not null,
-    dob DATE not null,
     created_date TIMESTAMP,
-    password VARCHAR(200) not null,
-    points INT
+    password VARCHAR(200) not null
 );
 
 CREATE TABLE quiz.category (
